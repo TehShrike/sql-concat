b6a907e823778b5db362dc5113981c4ff9d0f7fe
Josh Duff <me@JoshDuff.com>
2015-04-13 23:10:00 -0500

    A non-passing test

--- test.js
+++ test.js
@@ -27,3 +27,14 @@ test('select/from/where with params', function(t) {
 
 	t.end()
 })
+
+test('some joins', function(t) {
+	var result = q.select('wat')
+		.from('meh')
+		.join('no_in_fact_u', 'no_in_fact_u.meh_id = meh.id')
+		.join('who', 'who.no_in_fact_u_id = no_in_fact_u.id', 'LEFT')
+
+	t.equal(result.str, 'SELECT wat\nFROM meh\nJOIN no_in_fact_u ON no_in_fact_u.meh_id = meh.id\nLEFT JOIN who ON who.no_in_fact_u_id = no_in_fact_u.id')
+
+	t.end()
+})
